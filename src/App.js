c836085c3db027a5a91b2b04db2c8e7539787bc9
Ruben Kretek <rubenkretek@outlook.com>
2020-11-08 17:32:29 +0000

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+//Styles
+import "./styles/app.scss";
 //Components
-import Player from "./Components/Player";
-import Song from "./Components/Song";
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
   return (
