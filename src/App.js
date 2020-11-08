5d2abf16a4e3fcad688ceda344353750dc82fe64
Ruben Kretek <rubenkretek@outlook.com>
2020-11-08 11:24:00 +0000

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+//Components
+import Player from "./Components/Player";
+import Song from "./Components/Song";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
