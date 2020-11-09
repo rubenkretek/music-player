85fdbb88a9b075b2a5caa096aff0a7bc172ea7e5
Ruben Kretek <rubenkretek@outlook.com>
2020-11-09 07:58:40 +0000

    Added app data to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./styles/app.scss";
 //Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+//Import util
+import data from "./util";
 
 function App() {
   return (
